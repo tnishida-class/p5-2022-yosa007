3ea6780a1ad45c4d965d74a0d38c635797ee0e34
yosa007 <yoshisaito007@gmail.com>
2022-10-13 15:36:06 +0900

    section2-q2,3ver1
    途中です

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,13 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      if(i+j%2 == 0){
+      fill(2)}
+      else{
+      fill(0)}
+      rect(i*10,j*10,10,10)
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+      
   }
+}
 }
