2ad2a349b441000e79e107c92864726d45b85f86
yosa007 <yoshisaito007@gmail.com>
2022-10-16 23:07:52 +0900

    section2-q2 ver3

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,21 +5,21 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      if((i+j)%2 == 1 ) {
+      if((i+j)%2 == 1) {
       fill(196)}
-      else{
-      fill(256)}
-      rect(i*10,j*10,10,10)}
+      else{fill(256)}
+      rect(i*10,j*10,10,10)} //rect(i*10,j*10,10,10) ellipse(i*10+5,j*10+5,9)
     }
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      if((i+j)%2 == 1 && j < 3){
+      if((i+j)%2 == 1) {
+      fill(196) 
+      if(j<3) {
       fill(255,0,0)}
-      else if((i+j)%2 == 1 && j > 4) (fill(0))
-      else (fill (256))
-      ellipse(i*10+5,j*10+5,9)}
+      else if (j<5){
+      ;}
+      else{fill(0)}}
+      else{fill(256)}
+      ellipse(i*10+5,j*10+5,9)} //rect(i*10,j*10,10,10) ellipse(i*10+5,j*10+5,9)
     }
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-      
-  
-}
+  }
